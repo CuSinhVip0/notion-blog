a91f3588ca8bf63cce594825651f186085f848b3
CuSinhVip0 <sinhnguyen2k2@gmail.com>
2026-03-21 15:13:12 +0700

    fix: update params type in RootLayout to use Promise for better async handling

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -41,7 +41,7 @@ export default async function RootLayout({
     params,
 }: {
     children: React.ReactNode
-    params: { lang: string }
+    params: Promise<{ lang: string }>
 }) {
     const { lang } = await params
 
