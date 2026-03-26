78a27c21ffb38b10413bdf4aede9d1beef842034
CuSinhVip0 <sinhnguyen2k2@gmail.com>
2026-03-27 00:57:04 +0700

    Squashed commit of the following:
    commit 651fa8963aa77e707a59fd4b412ac026b1f2e1e7
    Merge: a37c76c 8d02ed8
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Fri Mar 27 00:37:42 2026 +0700
    
        Merge branch 'blog_csr' of https://github.com/CuSinhVip0/notion-blog into blog_csr
    
    commit 8d02ed85ad97cda1495369d1173b2a9caeca0016
    Merge: 0c71ff6 0570d0f
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Wed Mar 25 17:29:24 2026 +0700
    
        Refactor code structure for improved readability and maintainability
    
    commit 0c71ff6bcfa3d81c7e358bcd5546aeaa231e6b2b
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Wed Mar 25 14:48:14 2026 +0700
    
        feat: add AboutMe, Skills, WorkExperience, and ContactForm components with internationalization support
    
    commit 12b6ca186dac5028a226a28c7f96de4d8b5b1d7f
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Wed Mar 25 14:41:21 2026 +0700
    
        feat: enhance About page with new animations, internationalization support, and responsive design
    
    commit da1870114b081b79d2190e6401bf7e761a23dac9
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Wed Mar 25 10:41:03 2026 +0700
    
        feat: update About page content and enhance CurrentRole and DisplayName components
    
    commit bcb0ef3513824d0fde82d37945f667251858ca4f
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Wed Mar 25 10:37:48 2026 +0700
    
        feat: implement About page layout and enhance skills display with animations
    
    commit 0570d0fbf50ccd43e077bf19d484569cf284660d
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Tue Mar 24 11:30:43 2026 +0700
    
        fix: add .vscode to .gitignore to prevent IDE settings from being tracked
    
    commit 26c6ed522cd03430b97e6af436086491a6401b4c
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Tue Mar 24 11:29:02 2026 +0700
    
        feat: enhance internationalization support and add robots.txt for SEO
    
    commit e3652b7feafcc2dfcaa61b8dd5d341005a2ac18c
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Sat Mar 21 15:20:06 2026 +0700
    
        feat: add localization support with dynamic subtitle in navigation for multiple languages
    
    commit a91f3588ca8bf63cce594825651f186085f848b3
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Sat Mar 21 15:13:12 2026 +0700
    
        fix: update params type in RootLayout to use Promise for better async handling
    
    commit b2307b5e4fabbc3001829026199b16e36053a923
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Sat Mar 21 14:42:07 2026 +0700
    
        feat: add launch configuration and settings for integrated browser support
    
    commit b5ccaa6d37237b7877211ebec0671344803d9711
    Author: CuSinhVip0 <sinhnguyen2k2@gmail.com>
    Date:   Fri Mar 20 11:10:20 2026 +0700
    
        feat: integrate BlogContent component into BlogPostPage for enhanced post rendering

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -114,13 +114,13 @@ export default function PostHeader({ post }: PostHeaderProps) {
             )}
 
             {/* Content */}
-            <motion.div
+            {/* <motion.div
                 initial={{ opacity: 0, y: 16 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.3 }}
             >
                 <BlogContent recordMap={post.recordMap} />
-            </motion.div>
+            </motion.div> */}
         </>
     )
 }
